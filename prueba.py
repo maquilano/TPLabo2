12f5de5efcca8f6b4485f313bc0639e49d901eb9
Matias Aquilano <aquilanomatias@gmail.com>
2025-06-09 20:12:59 -0300

    imagen agregada
    imagen agregada

---
+++ prueba.py
@@ -0,0 +1,56 @@
+import pygame
+import chess
+
+# Configuración básica
+ANCHO_VENTANA = 640
+ALTO_VENTANA = 640
+FILAS, COLUMNAS = 8, 8
+TAM_CELDA = ANCHO_VENTANA // COLUMNAS
+
+# Colores
+BLANCO = (245, 245, 245)
+GRIS = (125, 135, 150)
+AZUL = (50, 130, 200)
+
+
+# Inicializar Pygame
+pygame.init()
+ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
+pygame.display.set_caption("Tablero de Ajedrez 8x8")
+
+imagen_pieza = pygame.image.load('C:/TPLabo2/piezas imagenes/rey_blanco.png').convert_alpha()
+imagen_pieza = pygame.transform.scale(imagen_pieza, (TAM_CELDA, TAM_CELDA))  # ajustar tamaño a la celda
+
+# Control de ejecución
+running = True
+celda_seleccionada = None
+
+# Función para dibujar el tablero
+def dibujar_tablero():
+    for fila in range(FILAS):
+        for col in range(COLUMNAS):
+            color = BLANCO if (fila + col) % 2 == 0 else GRIS
+            rect = pygame.Rect(col * TAM_CELDA, fila * TAM_CELDA, TAM_CELDA, TAM_CELDA)
+            pygame.draw.rect(ventana, color, rect)
+
+    if celda_seleccionada:
+        fila, col = celda_seleccionada
+        rect = pygame.Rect(col * TAM_CELDA, fila * TAM_CELDA, TAM_CELDA, TAM_CELDA)
+        pygame.draw.rect(ventana, AZUL, rect, 4)  # borde azul
+        ventana.blit(imagen_pieza, (col * TAM_CELDA, fila * TAM_CELDA))
+
+# Bucle principal
+while running:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            running = False
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            fila = y // TAM_CELDA
+            col = x // TAM_CELDA
+            celda_seleccionada = (fila, col)
+
+    dibujar_tablero()
+    pygame.display.flip()
+
+pygame.quit()
