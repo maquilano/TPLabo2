dc9b4725c884b53b55f7e148c55f15316fd838f0
Guito24 <diego.e.hamilton@gmail.com>
2025-06-14 15:45:49 -0300

    prueba -> main

--- main.py
+++ main.py
@@ -12,59 +12,55 @@ BLANCO = (245, 245, 245)
 GRIS = (125, 135, 150)
 AZUL = (50, 130, 200)
 
-
 # Inicializar Pygame
 pygame.init()
 ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
-pygame.display.set_caption("Tablero de Ajedrez 8x8")
+pygame.display.set_caption("Ajedrez en Python!")
 
-# Piezas
 
-# Blancas
-rey_blanco = pygame.image.load('piezas imagenes/rey_blanco.png')
-rey_blanco = pygame.transform.scale(rey_blanco, (TAM_CELDA, TAM_CELDA))  # ajusta tamaño a la celda
-reina_blanca = pygame.image.load('piezas imagenes/reina_blanco.png')
-reina_blanca = pygame.transform.scale(reina_blanca, (TAM_CELDA, TAM_CELDA))  
-alfil_blanco = pygame.image.load('piezas imagenes/alfil_blanco.png')
-alfil_blanco = pygame.transform.scale(alfil_blanco, (TAM_CELDA, TAM_CELDA)) 
-alfil_blanco2 = pygame.image.load('piezas imagenes/alfil_blanco.png')
-alfil_blanco2 = pygame.transform.scale(alfil_blanco2, (TAM_CELDA, TAM_CELDA))   
-torre_blanca = pygame.image.load('piezas imagenes/torre_blanco.png')
-torre_blanca = pygame.transform.scale(torre_blanca, (TAM_CELDA, TAM_CELDA))  
-torre_blanca2 = pygame.image.load('piezas imagenes/torre_blanco.png')
-torre_blanca2 = pygame.transform.scale(torre_blanca2, (TAM_CELDA, TAM_CELDA))
-caballo_blanco = pygame.image.load('piezas imagenes/caballo_blanco.png')
-caballo_blanco = pygame.transform.scale(caballo_blanco, (TAM_CELDA, TAM_CELDA))  
-caballo_blanco2 = pygame.image.load('piezas imagenes/caballo_blanco.png')
-caballo_blanco2 = pygame.transform.scale(caballo_blanco2, (TAM_CELDA, TAM_CELDA)) 
-peon_blanco = pygame.image.load('piezas imagenes/peon_blanco.png')
-peon_blanco = pygame.transform.scale(peon_blanco, (TAM_CELDA, TAM_CELDA))  
+def cargar_imagenes(nombre_img):
+    imagen = pygame.image.load(f"piezas imagenes/{nombre_img}.png").convert_alpha()
+    imagen_escalada = pygame.transform.scale(imagen, (TAM_CELDA, TAM_CELDA))
+    return imagen_escalada
 
-#negras
-rey_negro = pygame.image.load('piezas imagenes/rey_negro.png')
-rey_negro = pygame.transform.scale(rey_negro, (TAM_CELDA, TAM_CELDA)) 
-reina_negra = pygame.image.load('piezas imagenes/reina_negro.png')
-reina_negra = pygame.transform.scale(reina_negra, (TAM_CELDA, TAM_CELDA))  
-alfil_negro = pygame.image.load('piezas imagenes/alfil_negro.png')
-alfil_negro = pygame.transform.scale(alfil_negro, (TAM_CELDA, TAM_CELDA))  
-alfil_negro2 = pygame.image.load('piezas imagenes/alfil_negro.png')
-alfil_negro2 = pygame.transform.scale(alfil_negro2, (TAM_CELDA, TAM_CELDA))
-torre_negra = pygame.image.load('piezas imagenes/torre_negro.png')
-torre_negra = pygame.transform.scale(torre_negra, (TAM_CELDA, TAM_CELDA))
-torre_negra2 = pygame.image.load('piezas imagenes/torre_negro.png')
-torre_negra2 = pygame.transform.scale(torre_negra2, (TAM_CELDA, TAM_CELDA))  
-caballo_negro = pygame.image.load('piezas imagenes/caballo_negro.png')
-caballo_negro = pygame.transform.scale(caballo_negro, (TAM_CELDA, TAM_CELDA))
-caballo_negro2 = pygame.image.load('piezas imagenes/caballo_negro.png')
-caballo_negro2 = pygame.transform.scale(caballo_negro, (TAM_CELDA, TAM_CELDA))  
-peon_negro = pygame.image.load('piezas imagenes/peon_negro.png')
-peon_negro = pygame.transform.scale(peon_negro, (TAM_CELDA, TAM_CELDA))  
+#diccionario que va a usar cargar_imagenes para cargar y reescalar cada imagen
+Nombres_piezas = {
+    "rb": "rey_blanco",
+    "db": "reina_blanco",
+    "ab": "alfil_blanco",
+    "tb": "torre_blanco",
+    "cb": "caballo_blanco",
+    "pb": "peon_blanco",
+    "rn": "rey_negro",
+    "dn": "reina_negro",
+    "an": "alfil_negro",
+    "tn": "torre_negro",
+    "cn": "caballo_negro",
+    "pn": "peon_negro",
+}
+
+#diccionario donde se van a cargar como clave la abreviacion de la pieza y como valor la imagen reescalada
+PIEZAS = {}
+
+for clave, nombre in Nombres_piezas.items(): #clave = rb, nombre = rey_blanco
+    imagen = cargar_imagenes(nombre)
+    PIEZAS[clave] = imagen
+
+# Posiciones iniciales de las piezas (pieza, columna, fila)
+posiciones_piezas = [
+    ("tn", 0, 0), ("cn", 1, 0), ("an", 2, 0), ("dn", 3, 0), ("rn", 4, 0), ("an", 5, 0), ("cn", 6, 0), ("tn", 7, 0),
+    *[("pn", i, 1) for i in range(8)],
+    *[("pb", i, 6) for i in range(8)],
+    ("tb", 0, 7), ("cb", 1, 7), ("ab", 2, 7), ("db", 3, 7), ("rb", 4, 7), ("ab", 5, 7), ("cb", 6, 7), ("tb", 7, 7),
+]
 
 # Control de ejecución
 running = True
+
+#Variables de control
+pieza_seleccionada = None 
 celda_seleccionada = None
 
-# Función para dibujar el tablero
 def dibujar_tablero():
     for fila in range(FILAS):
         for col in range(COLUMNAS):
@@ -75,16 +71,21 @@ def dibujar_tablero():
     if celda_seleccionada:
         fila, col = celda_seleccionada
         rect = pygame.Rect(col * TAM_CELDA, fila * TAM_CELDA, TAM_CELDA, TAM_CELDA)
-        pygame.draw.rect(ventana, AZUL, rect, 4)  # borde azul
-        ventana.blit(alfil_negro, (col * TAM_CELDA, fila * TAM_CELDA))
+        pygame.draw.rect(ventana, AZUL, rect, 4)
+
+    # Dibujar piezas
+    for pieza, col, fila in posiciones_piezas:
+        if pieza in PIEZAS:
+            ventana.blit(PIEZAS[pieza], (col * TAM_CELDA, fila * TAM_CELDA))
 
 # Bucle principal
 while running:
     for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
+        if evento.type == pygame.QUIT: #fin del while si user cierra el programa 
             running = False
-        elif evento.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
+
+        elif evento.type == pygame.MOUSEBUTTONDOWN: #Detecto si el user hizo click en la ventana del juego
+            x, y = pygame.mouse.get_pos() #Obtenemos cordenadas del click para luego identidicar la celda seleccionada
             fila = y // TAM_CELDA
             col = x // TAM_CELDA
             celda_seleccionada = (fila, col)
